239d42a0f2a24698882d2ad453d7b9408253f421
aquariusmaster <aquariusmaster@yandex.ru>
2016-10-08 14:13:14 +0300

    change Column name for db

--- src/main/java/com/karienomen/teach/hibertest/domain/UserDetails.java
+++ src/main/java/com/karienomen/teach/hibertest/domain/UserDetails.java
@@ -1,16 +1,19 @@
 package com.karienomen.teach.hibertest.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 /**
  * Created by karienomen on 08.10.16.
  */
-@Entity
+@Entity(name = "USERS")
 public class UserDetails {
 
     @Id
+    @Column(name = "USER_ID")
     private int userId;
+    @Column(name = "USERNAME")
     private String userName;
 
     public int getUserId() {
