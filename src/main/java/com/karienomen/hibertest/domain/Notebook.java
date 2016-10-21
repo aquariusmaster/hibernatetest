23b3848ef33b96c6aaa234b9421ebf5af0d23220
aquariusmaster <aquariusmaster@yandex.ru>
2016-10-21 13:51:49 +0300

    create new class Notebook extends Computer

---
+++ src/main/java/com/karienomen/hibertest/domain/Notebook.java
@@ -0,0 +1,17 @@
+package com.karienomen.hibertest.domain;
+
+/**
+ * Created by karienomen on 21.10.16.
+ */
+public class Notebook extends Computer {
+
+    private float screenSize;
+
+    public float getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(float screenSize) {
+        this.screenSize = screenSize;
+    }
+}
